3622ae527b57bf9dc92e87f18e2d215c25f936b0
Ankit Das <ecryptoguru@gmail.com>
2023-10-23 01:09:47 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Ankit-GPT',
-  description: 'Created by Ankit Das',
+  title: 'FusionChat',
+  description: 'FusionwaveAI GPT',
 }
 
 export default function RootLayout({
